fb2aaa5149ee795dbd55a7a74ede3e63ea7705ab
AdrianTheFrog <afsw759@gmail.com>
2024-05-30 15:55:06 -0400

    player animations

--- Player.gd
+++ Player.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
+var playerForwards = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	%PlayerSprite.frame = 0
+	%PlayerArm.frame = 0
+	%PlayerArm.frame_progress = 0.0
+	%PlayerSprite.frame_progress = 0.0
+	%PlayerSprite.play("default")
+	%PlayerArm.play("default")
 
 var jumpCooldown = 0
 
@@ -25,3 +31,22 @@ func _physics_process(delta):
 		if Input.is_action_pressed("Right"):
 			apply_force(Vector2(50000,0)*delta)
 		set_axis_velocity(Vector2(linear_velocity.x*0.95,linear_velocity.y))
+	if Input.is_action_just_pressed("Left"):
+		%PlayerSprite.play("flipped")
+		%PlayerArm.play("flipped")
+		playerForwards = false
+		%PlayerArm.position.x = 0
+	if Input.is_action_just_pressed("Right"):
+		%PlayerSprite.play("default")
+		%PlayerArm.play("default")
+		%PlayerArm.position.x = 0
+		playerForwards = true
+	if Input.is_action_just_pressed("Attack"):
+		%PlayerSprite.frame = 1
+		%PlayerSprite.pause()
+		if playerForwards:
+			%PlayerArm.play("stab")
+			%PlayerArm.position.x += 8
+		else:
+			%PlayerArm.play("stab flipped")
+			%PlayerArm.position.x -= 8
